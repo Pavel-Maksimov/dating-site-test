fef3fa4b3584e34702a1a88ccf4fce9e0ed09b41
Pavel Maksimov <maksimov.pavel.an@yandex.ru>
2022-03-26 03:24:00 +0500

    Update settings.py

--- dating/dating/settings.py
+++ dating/dating/settings.py
@@ -11,11 +11,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '---')
 
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = [
-    os.environ.get('ALLOWED_HOSTS'),
-    'localhost',
-    '127.0.0.1',
-]
+ALLOWED_HOSTS = [*]
 
 AUTH_USER_MODEL = 'clients.Client'
 
