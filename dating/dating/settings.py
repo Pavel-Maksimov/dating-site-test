4afefdbf88bfb5d4b43950232829184a0827ef68
Pavel Maksimov <maksimov.pavel.an@yandex.ru>
2022-03-28 22:06:56 +0500

    Update settings.py

--- dating/dating/settings.py
+++ dating/dating/settings.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 
 from dotenv import load_dotenv
+import dj_database_url
 
 load_dotenv()
 
@@ -62,10 +63,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'dating.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 AUTH_PASSWORD_VALIDATORS = [
